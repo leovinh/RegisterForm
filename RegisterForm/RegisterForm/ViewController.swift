52247259c95ab6c4f7dad19c18ce14d835cf2902
Kevin Duong <kelvin.timsach@gmail.com>
2015-11-06 23:13:25 +0700

    Done normal

--- RegisterForm/RegisterForm/ViewController.swift
+++ RegisterForm/RegisterForm/ViewController.swift
@@ -10,9 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtName: UITextField!
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var txtPhoneNumber: UITextField!
+    @IBOutlet weak var txtvResult: UITextView!
+    
+    @IBAction func Show(sender: AnyObject) {
+        let name:String = txtName.text!
+        let email:String = txtEmail.text!
+        let phonenumber:String = txtPhoneNumber.text!
+        
+        let result:String = "Register information is: \nName:" + name + "\nEmail:" + email + "\nPhone Number:" + phonenumber
+       
+        txtvResult.text = result
+        
+        
+    }
+    
+    
+    
+    @IBAction func Delete(sender: AnyObject) {
+        txtName.text = ""
+        txtEmail.text = ""
+        txtPhoneNumber.text = ""
+        txtvResult.text = ""
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
